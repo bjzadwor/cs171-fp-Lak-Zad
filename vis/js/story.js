30c16ce7fefdb02c4dd9055c84c59913523f313b
Bryan Zadworney <bjzadwor@hotmail.com>
2014-05-01 07:39:26 -0400

    Working on positioniong and code for Storytelling

--- vis/js/story.js
+++ vis/js/story.js
@@ -2,6 +2,25 @@
  * Created by Zadworney on 4/30/2014.
  */
 
-function story(){
+function story(input){
+console.log(input);
+divID = "#"+input;
+    console.log(divID);
+    $("#"+input).addClass("hidden");
 
+    switch (input){
+        case "":
+            break;
+
+
+        default:
+        break;
+
+
+    }
+}
+
+function exitInstructions(){
+    console.log("Exiting Instructions")
+    $('.allInstructions').addClass("hidden");
 }
